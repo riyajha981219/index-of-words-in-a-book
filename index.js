97161f36e897402a6460c30009d7517daac72d9b
Riya <jhariya.1912@gmail.com>
2023-06-22 12:23:19 +0530

    Enhanced code

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ class BookIndexer {
     constructor() {
         this.pages = [];
         this.excludeWords = [];
-        this.wordIndex = {};
+        this.wordIndex = new Map();
     }
 
+    //read all the pages
     readPages(pageFiles) {
         for (const file of pageFiles) {
             const content = readFileSync(file, 'utf8');
@@ -15,6 +16,7 @@ class BookIndexer {
         }
     }
 
+    //read the words that should be ignored 
     readExcludeWords(excludeWordsFile) {
         const content = readFileSync(excludeWordsFile, 'utf8');
         this.excludeWords = content.split(/\s+/);
@@ -25,52 +27,19 @@ class BookIndexer {
             const pageWords = this.pages[i];
             for (const word of pageWords) {
                 if (!this.excludeWords.includes(word)) {
-                    if (!this.wordIndex[word]) {
-                        this.wordIndex[word] = [];
+                    if (!this.wordIndex.has(word)) {
+                        this.wordIndex.set(word, []);
                     }
-        
-                    // Binary search implementation
-                    const pageIndex = this.binarySearch(this.wordIndex[word], i + 1);
-        
-                    if (pageIndex === -1) {
-                        // Insert the page number at the correct position
-                        this.wordIndex[word].splice(-(pageIndex + 1), 0, i + 1);
-                    }
+
+                    // Insert the page number at the correct position
+                    this.wordIndex.get(word).push(i + 1);
                 }
             }
         }
-    }
-
-    binarySearch(array, target) {
-        let left = 0;
-        let right = array.length - 1;
-  
-        while (left <= right) {
-            const mid = Math.floor((left + right) / 2);
-    
-            if (array[mid] === target) {
-                return mid; // Target found
-            } else if (array[mid] < target) {
-                left = mid + 1; // Target is in the right half
-            } else {
-                right = mid - 1; // Target is in the left half
-            }
-        }
-  
-        return -1; // Target not found
     }
 
     generateIndexFile(outputFile) {
-        const sortedWords = Object.keys(this.wordIndex);
-  
-        // Bubble sort implementation
-        for (let i = 0; i < sortedWords.length - 1; i++) {
-            for (let j = 0; j < sortedWords.length - i - 1; j++) {
-                if (sortedWords[j] > sortedWords[j + 1]) {
-                    [sortedWords[j], sortedWords[j + 1]] = [sortedWords[j + 1], sortedWords[j]]; // Swap
-                }
-            }
-        }
+        const sortedWords = Array.from(this.wordIndex.keys()).sort();
   
         let output = '';
         for (const word of sortedWords) {
@@ -80,12 +49,11 @@ class BookIndexer {
         writeFileSync(outputFile, output);
     }
 }
-
 // Usage example
 const bookIndexer = new BookIndexer();
-const pageFiles = ["E:/work-folder/Portfolio/wordSearch/files/Page1.txt","E:/work-folder/Portfolio/wordSearch/files/Page2.txt","E:/work-folder/Portfolio/wordSearch/files/Page3.txt"];
-const excludeWordsFile = 'E:/work-folder/Portfolio/wordSearch/files/exclude-words.txt';
-const outputFile = 'E:/work-folder/Portfolio/wordSearch/files/index.txt';
+const pageFiles = ["./files/Page1.txt","./files/Page2.txt","./files/Page3.txt,"];
+const excludeWordsFile = './files/exclude-words.txt';
+const outputFile = './files/index.txt';
 
 bookIndexer.readPages(pageFiles);
 bookIndexer.readExcludeWords(excludeWordsFile);
